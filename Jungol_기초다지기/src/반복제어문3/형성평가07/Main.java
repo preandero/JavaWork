c758c9df54cf0903a53efbaf5ae8b68d069e9db4
preandero <preandero@gmail.com>
2020-05-06 20:47:38 +0900

    Mac push 20/05/06

--- Jungol_기초다지기/src/반복제어문3/형성평가07/Main.java
+++ Jungol_기초다지기/src/반복제어문3/형성평가07/Main.java
@@ -1,29 +1,54 @@
 package 반복제어문3.형성평가07;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println(Solution.solution(10));
 		
-		parse("invalid");
 	}
 
-	public static void parse(String str) {
-
-		float f = Float.parseFloat(str);
-		try {
-
-		} catch (NumberFormatException nfe) {
-
-			f = 0;
-
-		} finally {
-
-			System.out.println(f);
-
-		}
-
-	}
+	
 }
+
+
+class Solution {
+	  public static long solution(long n) {
+	      	long answer = 0;
+	      
+	      	String ia = Long.toString(n);
+	      	
+	      	ArrayList<Long> result = new ArrayList<Long>();
+	      	
+	      	for(int i =0 ; i < result.size();i++) {
+	      		long lo = Long.parseLong(ia, ia.charAt(i));
+	      		result.add(lo);
+	      		System.out.println(result.get(i));
+	      	}
+	      	
+	      	
+	      	Collections.sort(result,Collections.reverseOrder());
+	      	
+	      	
+	      	
+	      	
+	      	for(int i =0 ; i < result.size(); i++) {
+	      		System.out.println(result.get(i));
+	      		
+	      		
+	      	}
+	      	
+	      
+	     
+	      	
+	      	
+	      
+	      
+	      return answer;
+	  }
+	}
